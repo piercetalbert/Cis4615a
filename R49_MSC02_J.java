29b421313da5c9517568b61b98aac799eaf0cd0b
Pierce Talbert <piercetalbert@outlook.com>
2019-11-07 16:54:48 -0500

    Add files via upload

--- R49_MSC02_J.java
+++ R49_MSC02_J.java
@@ -2,22 +2,28 @@
  *  Compilation:  javac R49_MSC02_J.java
  *  Execution:    java R49_MSC02_J
  *
- *  Calls function getAbsAdd(x,y) with and without using assertions.
+ *  Generates random numbers in a secure fashion using SecureRandom
  *
  ******************************************************************************/
 
-import java.util.Random;
+import java.security.SecureRandom;
+import java.security.NoSuchAlgorithmException;
 
 public class R49_MSC02_J {
 
+  /*
+  * Rule 49. Miscellaneous (MSC)
+  * Corrected code per:
+  * https://wiki.sei.cmu.edu/confluence/display/java/MSC02-J.+Generate+strong+random+numbers
+  *
+  */
+
   public static void main(String[] args) {
 
-    Random number = new Random(123L);
-    //...
+    SecureRandom number = new SecureRandom();
+    // Generate 20 integers 0..20
     for (int i = 0; i < 20; i++) {
-      // Generate another random integer in the range [0, 20]
-      int n = number.nextInt(21);
-      System.out.println(n);
+      System.out.println(number.nextInt(21));
     }
   }
 }
