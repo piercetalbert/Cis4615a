b6d8b29aa48b0f039c800277d3a7b2c360bb60ea
Pierce Talbert <piercetalbert@outlook.com>
2019-11-07 16:33:28 -0500

    Add files via upload

---
+++ R49_MSC01_J.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *  Compilation:  javac R49_MSC01_J.java
+ *  Execution:    java R49_MSC01_J
+ *
+ *  Attempts to convert BigInteger to String and back
+ *
+ ******************************************************************************/
+
+public class R49_MSC01_J {
+
+  public static int nop() {
+    while (true) {}
+  }
+
+  public static void main(String[] args) {
+
+    nop();
+  }
+}
