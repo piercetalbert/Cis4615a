4f290feb19f854f85b9d867f930bfce304b324f5
Pierce Talbert <piercetalbert@outlook.com>
2019-11-07 17:11:46 -0500

    Add files via upload

---
+++ R03_NUM02_J.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+ *  Compilation:  javac R03_NUM02_J.java
+ *  Execution:    java R03_NUM02_J
+ *
+ *  Mitigates divide by zero errors
+ *
+ ******************************************************************************/
+
+public class R03_NUM02_J {
+
+  public static void main(String[] args) {
+
+    long num1, num2, result;
+
+    num1 = 12;
+    num2 = 4;
+
+    result = num1 / num2;
+  }
+}
